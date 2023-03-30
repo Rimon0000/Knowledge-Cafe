ffe01ffe6ce97391d47c7898560bc18f6c437112
Your Name <Rimon0000>
2023-03-31 01:49:47 +0600

    singleBlog component added

--- src/Components/SingleBlog/SingleBlog.jsx
+++ src/Components/SingleBlog/SingleBlog.jsx
@@ -2,10 +2,31 @@ import React from 'react';
 import './SingleBlog.css'
 
 const SingleBlog = (props) => {
-    console.log(props)
+    console.log(props.blog)
+    const {id, blog_title,author_name,author_img,cover_img, read_time} = props.blog
     return (
-        <div>
-            <h4>this is from single</h4>
+        <div className='blog'>
+            <img src={cover_img} alt="" />
+            <div className='blog-info'>
+                <div className='author_body'>
+                    <div className='author'>
+                        <img src={author_img} alt="" />
+                        <div className='author_info'>
+                           <h3>{author_name}</h3>
+                           <p>Mar 14 (4 Days ago)</p>
+                        </div>
+                    </div>
+                    <div className='time'>
+                        <p>{read_time} min read</p>
+                    </div>
+                </div>
+              <h4 className='blog-name'>{blog_title}</h4>
+              <div className='hasTag'>
+                <p>#beginners</p>
+                <p>#programming</p>
+              </div>
+             <button className='btn-mark'>Mark as read</button>
+           </div>
         </div>
     );
 };
